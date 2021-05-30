6f0f78eba746ed45ae2dd42fe6458949eddea331
dev-lincoln <lincoln.dev@outlook.com>
2021-05-30 13:57:26 -0300

    exercio completo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 const port = 3000
 const usuarioFernando = {
-    id: 1,
+    id: 0,
     nome: 'Fernando',
     idade: 35
 };
@@ -13,8 +13,44 @@ const usuarioFernando = {
 const usuarios = [usuarioFernando];
 
 app.get('/usuarios', (req, res) => {
-    // res.send(JSON.stringify(usuarios))
-    res.json(usuarios)
+    // res.send(JSON.stringify(usuarios)) 
+    res.json(usuarios) // melhor usar esse quando for retornar json 
+});
+
+app.post('/usuarios', (req, res) => {
+    const { nome, idade } = req.body;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id));
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuario)
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    usuarios.splice(index, 1);
+
+    res.status(204).send();
+    
 });
 
 app.listen(3000, () => {
